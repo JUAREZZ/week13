e09ed61d1760ee432e30c34c09f312fb203d5049
JUAREZZ <35316562+JUAREZZ@users.noreply.github.com>
2018-04-08 21:27:21 -0500

    Create chapter12.sh

---
+++ chapter12.sh
@@ -0,0 +1,24 @@
+#!/bin/bash 
+
+# Chapter12
+
+#create to files
+
+touch week13 april08
+
+#rsync files, 
+
+rsync week13 april08...host
+
+#type in data required and password
+
+adduser juarezz
+
+
+#transfer directory 
+
+rsync -a dir host:destination_dir
+
+#copy directory
+
+ rsync -nva dir host:destination_dir
